abe432f017fef488f1ebd27a02dad372bd84df42
Tony Ochiche <aochiche@msn.com>
2018-12-31 09:20:49 -0600

    Add dbcontext class

---
+++ CTCore/CTCore.Data/CTCoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CTCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTCore.Data
+{
+    public class CTCoreDbContext : DbContext
+    {
+        public DbSet<StudentClass> StudentClass { get; set; }
+        public DbSet<ClassType> ClassType { get; set; }
+        public DbSet<Instructor> Instructor { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Database=CTCore;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
