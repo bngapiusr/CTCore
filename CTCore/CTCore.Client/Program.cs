783580dec4d69c2ccc1a6e420f82eeb13586efce
Tony Ochiche <aochiche@msn.com>
2018-12-31 16:37:22 -0600

    Add program class...

---
+++ CTCore/CTCore.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CTCore.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
