71a2b5781b197b0ad27ef1f88748bd79f9eb6281
Tony Ochiche <aochiche@msn.com>
2019-01-02 06:34:04 -0600

    Add Logging to console...

--- CTCore/CTCore.Data/CTCoreDbContext.cs
+++ CTCore/CTCore.Data/CTCoreDbContext.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using System.Text;
 using CTCore.Domain;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 namespace CTCore.Data
 {
     public class CtCoreDbContext : DbContext
     {
+        //Logger factory logs messages to the console app
+
+        public static readonly LoggerFactory CtCoreConsoleLoggerFactory = new LoggerFactory(new[]
+        {
+            new ConsoleLoggerProvider((category, level)
+                => category==DbLoggerCategory.Database.Command.Name
+                   && level == LogLevel.Information, true)
+        });
+
+
         public DbSet<StudentClass> StudentClass { get; set; }
         public DbSet<ClassType> ClassType { get; set; }
         public DbSet<Instructor> Instructor { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=.;Database=CTCore;Trusted_Connection=True;");
+            optionsBuilder
+                .UseLoggerFactory(CtCoreConsoleLoggerFactory)
+                .UseSqlServer(@"Data Source=.;Database=CTCore;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
